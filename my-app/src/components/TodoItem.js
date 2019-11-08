61f8c96a0fe9100cf3ae4cf6d4f916c0a1e81e90
gsavickas <46458704+gsavickas@users.noreply.github.com>
2019-11-08 13:31:39 -0600

    Updates to TodoItem and Todos

--- my-app/src/components/TodoItem.js
+++ my-app/src/components/TodoItem.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import PropTyes from 'prop-types';
 
 export class TodoItem extends Component {
+
+  getStyle = () => {
+    return {
+      
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div style={this.getStyle()}>
         <p>{ this.props.todos.title }</p>
       </div>
     )
   }
 }
+
+// PropTypes
+TodoItem.propTypes = {
+  todos: PropTyes.object.isRequired
+}
+
 
 export default TodoItem
